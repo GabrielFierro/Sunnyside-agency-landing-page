979993daf0d3be5d13f13558522beba4f8ab0ce1
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-04-25 19:54:25 -0300

    add: the nav component with a list of anchors to sections

---
+++ src/components/navbar/Nav.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <ul className='nav'>
+      <li>
+        <a href='#'>About</a>
+      </li>
+      <li>
+        <a href='#'>Services</a>
+      </li>
+      <li>
+        <a href='#'>Projects</a>
+      </li>
+      <li>
+        <a href='#'>Contact</a>
+      </li>
+    </ul>
+  )
+}
