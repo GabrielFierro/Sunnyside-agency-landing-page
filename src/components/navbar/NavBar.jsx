9b05a4e0eaa1f242ee9131ea88464aa9ecf936ec
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-04-25 19:57:44 -0300

    fix: the imports for AgencyLogo and the App.css

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import AgencyLogo from '../assets/images/logo.svg'
+import AgencyLogo from '../../assets/images/logo.svg'
 import Nav from './Nav'
-import '../App.css'
+import '../../App.css'
 
 export default function NavBar() {
   return (
