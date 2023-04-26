19cf86fe47612bc4fed41e2cdc18583a8f3e10ea
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-04-26 19:39:01 -0300

    add: the Image, Text, and ImageTransform as components with his props

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,41 @@
 import NavBar from './components/navbar/NavBar'
+import Image from './components/Card/Image'
+import Text from './components/Card/Text'
+import ImageTransform from './assets/images/desktop/image-transform.jpg'
 import './App.css'
 
 export default function App() {
   return (
     <>
       <NavBar />
-      Transform your brand We are a full-service creative agency specializing in
-      helping brands grow fast. Engage your clients through compelling visuals
-      that do most of the marketing for you. Learn more Stand out to the right
-      audience Using a collaborative formula of designers, researchers,
-      photographers, videographers, and copywriters, we'll build and extend your
-      brand in digital places. Learn more Graphic design Great design makes you
-      memorable. We deliver artwork that underscores your brand message and
-      captures potential clients' attention. Photography Increase your
-      credibility by getting the most stunning, high-quality photos that improve
-      your business image. Client testimonials We put our trust in Sunnyside and
-      they delivered, making sure our needs were met and deadlines were always
-      hit. Emily R. Marketing Director Sunnyside's enthusiasm coupled with their
-      keen interest in our brand's success made it a satisfying and enjoyable
-      experience. Thomas S. Chief Operating Officer Incredible end result! Our
-      sales increased over 400% when we worked with Sunnyside. Highly
-      recommended! Jennie F. Business Owner About Services Projects Contact
+      <div className='grid_container'>
+        <Text
+          text={{
+            title: 'Transform your brand',
+            paragraph:
+              'We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.',
+            span: 'Learn more'
+          }}
+        />
+        <Image
+          src={ImageTransform}
+          alt='Image about an egg with yellow background'
+        />
+      </div>
+      Stand out to the right audience Using a collaborative formula of
+      designers, researchers, photographers, videographers, and copywriters,
+      we'll build and extend your brand in digital places. Learn more Graphic
+      design Great design makes you memorable. We deliver artwork that
+      underscores your brand message and captures potential clients' attention.
+      Photography Increase your credibility by getting the most stunning,
+      high-quality photos that improve your business image. Client testimonials
+      We put our trust in Sunnyside and they delivered, making sure our needs
+      were met and deadlines were always hit. Emily R. Marketing Director
+      Sunnyside's enthusiasm coupled with their keen interest in our brand's
+      success made it a satisfying and enjoyable experience. Thomas S. Chief
+      Operating Officer Incredible end result! Our sales increased over 400%
+      when we worked with Sunnyside. Highly recommended! Jennie F. Business
+      Owner About Services Projects Contact
     </>
   )
 }
