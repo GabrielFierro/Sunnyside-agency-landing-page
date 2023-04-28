651a6570ff7f32eb0e58a856a5e96e477d8e4535
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-04-28 18:12:49 -0300

    add: the Header component who contain a title and an image

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ArrowDown from '../../assets/images/icon-arrow-down.svg'
+import '../../App.css'
+
+export default function Header({ title }) {
+  return (
+    <div>
+      <h1 className='title'>{title}</h1>
+      <div className='container_arrow'>
+        <img src={ArrowDown} alt='arrow down' />
+      </div>
+    </div>
+  )
+}
