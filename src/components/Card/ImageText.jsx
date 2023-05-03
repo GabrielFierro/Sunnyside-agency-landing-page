86fc2627c947ef555405b202491c650be7e95c6c
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-05-03 20:04:19 -0300

    add: a component who has an image with text center

---
+++ src/components/Card/ImageText.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function ImageText({ text, src, alt }) {
+  return (
+    <div>
+      <img className='image' src={src} alt={alt} />
+      <div className='center'>
+        <h3>{text.title}</h3>
+        <p>{text.paragraph}</p>
+        <span>{text.span}</span>
+      </div>
+    </div>
+  )
+}
