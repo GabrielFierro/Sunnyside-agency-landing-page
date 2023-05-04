31ad7fb26fe45faf7c95a597ec6c419b139026cf
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-05-03 23:42:09 -0300

    add: the prop isGreen and display a text in green or in blue color

--- src/components/Card/ImageText.jsx
+++ src/components/Card/ImageText.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 
-export default function ImageText({ text, src, alt }) {
+export default function ImageText({ text, src, alt, isGreen }) {
   return (
     <div className='containerImage'>
       <img className='image' src={src} alt={alt} />
       <div className='centerContainer'>
         <div className='centerText'>
-          <h3>{text.title}</h3>
-          <p>{text.paragraph}</p>
+          {isGreen ? (
+            <div className='styleText green'>
+              <h3>{text.title}</h3>
+              <p>{text.paragraph}</p>
+            </div>
+          ) : (
+            <div className='styleText blue'>
+              <h3>{text.title}</h3>
+              <p>{text.paragraph}</p>
+            </div>
+          )}
         </div>
       </div>
     </div>
