43b572620bb72e52ca63f49ec60d5449d1759bab
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-05-03 21:41:04 -0300

    add: the prop isYellow to display with a conditional rendering the span element

--- src/components/Card/Text.jsx
+++ src/components/Card/Text.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
-export default function Text({ text }) {
+export default function Text({ text, isYellow }) {
   return (
     <div className='containerText'>
       <div className='text'>
         <h3>{text.title}</h3>
         <p>{text.paragraph}</p>
-        <span>{text.span}</span>
+        <div>
+          {isYellow ? (
+            <span className='underline_yellow'>{text.span}</span>
+          ) : (
+            <span className='underline_red'>{text.span}</span>
+          )}
+        </div>
       </div>
     </div>
   );
