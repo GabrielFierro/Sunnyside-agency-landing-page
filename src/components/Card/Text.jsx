b8b90d8d89e4ea8f072d81f97647ce8daf77b8ea
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-05-03 21:24:36 -0300

    fix: the name of the prop className

--- src/components/Card/Text.jsx
+++ src/components/Card/Text.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Text({ text }) {
   return (
     <div className='containerText'>
-      <div class='text'>
+      <div className='text'>
         <h3>{text.title}</h3>
         <p>{text.paragraph}</p>
         <span>{text.span}</span>
