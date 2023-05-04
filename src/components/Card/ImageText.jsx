a19fe354cb8eaf1b818c3e59033d6e1d4bcf5fb4
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-05-03 23:23:39 -0300

    add: the centerContainer, and the centerText classes

--- src/components/Card/ImageText.jsx
+++ src/components/Card/ImageText.jsx
@@ -4,10 +4,12 @@ export default function ImageText({ text, src, alt }) {
   return (
     <div className='containerImage'>
       <img className='image' src={src} alt={alt} />
-      <div className='center'>
-        <h3>{text.title}</h3>
-        <p>{text.paragraph}</p>
-        <span>{text.span}</span>
+      <div className='centerContainer'>
+        <div className='centerText'>
+          <h3>{text.title}</h3>
+          <p>{text.paragraph}</p>
+          <span>{text.span}</span>
+        </div>
       </div>
     </div>
   );
