cabd927f9dba626c17e8d751ca6ef0559c30a3e9
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-05-03 21:18:34 -0300

    add: a div container, the containerText class, and the text class

--- src/components/Card/Text.jsx
+++ src/components/Card/Text.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 export default function Text({ text }) {
   return (
-    <div>
-      <h3>{text.title}</h3>
-      <p>{text.paragraph}</p>
-      <span>{text.span}</span>
+    <div className='containerText'>
+      <div class='text'>
+        <h3>{text.title}</h3>
+        <p>{text.paragraph}</p>
+        <span>{text.span}</span>
+      </div>
     </div>
   )
 }
