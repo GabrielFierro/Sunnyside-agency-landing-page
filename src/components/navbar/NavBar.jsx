b033eea83be8726873c0592d0097d567653d3ce3
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-05-05 12:22:51 -0300

    add: the hamburger menu

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -11,6 +11,15 @@ export default function NavBar() {
         <div className='navbar'>
           <img className='logo' src={AgencyLogo} alt='Logo' />
           <Nav />
+          <div className='hamburger'>
+            <svg width='24' height='18' xmlns='http://www.w3.org/2000/svg'>
+              <path
+                d='M24 16v2H0v-2h24zm0-8v2H0V8h24zm0-8v2H0V0h24z'
+                fill='#FFF'
+                fill-rule='evenodd'
+              />
+            </svg>
+          </div>
         </div>
       </div>
       <Header title='we are creative' />
